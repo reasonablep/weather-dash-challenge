d9cefd337bd4fa23bc27da2b05f0e986bcc78f71
reasonablep <140934359+reasonablep@users.noreply.github.com>
2023-09-07 01:39:56 -0700

    Added API

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let userContainer = document.getElementById('weather');
-let updateWeather = docuemnt.getElementById('update-button');
+let updateWeather = document.getElementById('update-button');
 
 function getApi() {
-    let requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key";
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={c3ae464095c2e4d49982c37fea88fd86}";
     
     fetch (requestUrl)
+    .then (function (response) {
+        return response.json();
+    })
 }
+    // .then (function (data {
+    //     console.log(data); 
+    //     for (let i=0; i < data.length; i++) {
+    //         let userData = document.createElement('h2');
+    //         userData.textContent = data [i]
+    //     }
+
+    // }
