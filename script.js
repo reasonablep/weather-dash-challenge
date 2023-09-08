fcd3c0a17c284ee6a0d15aef65148f8ea891afaf
reasonablep <140934359+reasonablep@users.noreply.github.com>
2023-09-08 01:39:18 -0700

    Added style, cleared console warnings, still won't update

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
-let userContainer = document.getElementById('weather');
-let updateWeather = document.getElementById('update-button');
+document.addEventListener('DOMContentLoaded', () => {
+    const getWeatherButton = document.getElementById('getWeather');
+    const cityInput = document.getElementById('cityName');
+    const weatherInfo = document.getElementById('weatherInfo');
 
-function getApi() {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={c3ae464095c2e4d49982c37fea88fd86}";
-    
-    fetch (requestUrl)
-    .then (function (response) {
-        return response.json();
+    getWeatherButton.addEventListener('click',() => {
+        const city = cityInput.value;
+
+        if (!city) {
+            alert ('Please enter a valid city name');
+            return;
+        }
+
+        const apiKey = 'WeatherAPI';
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={c3ae464095c2e4d49982c37fea88fd86}`;
+
+        function getWeather () {
+            fetch (weatherUrl)
+            .then ((response) => {
+                if (response.status === 200) {
+                return response.json();
+        }
+
+        else { 
+            throw new Error('Weather update failed.');
+        }
     })
-}
-    // .then (function (data {
-    //     console.log(data); 
-    //     for (let i=0; i < data.length; i++) {
-    //         let userData = document.createElement('h2');
-    //         userData.textContent = data [i]
-    //     }
+    .then ((data) => {
 
-    // }
+        const temperature = data.list[0].main.temp;
+        weatherInfo.innerHTML = 
+        `<h2>Weather in ${city}</h2>
+        <p>Temperature ${data-temperature}</p> "`;
+
+    })
+
+
+.catch((error) => {
+    console.error('Error getting weather data', error);
+
+        });
+
+    }
+
+
+     getWeather();
+      
+
+        });
+
+    });
